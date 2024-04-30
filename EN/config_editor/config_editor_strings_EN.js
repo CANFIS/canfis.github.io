b493f036d01ee943626a2c7cac76a0dfbbaf2364
domnulvlad <147989168+CANFIS@users.noreply.github.com>
2024-04-30 16:41:34 +0300

    3.0.0

---
+++ EN/config_editor/config_editor_strings_EN.js
@@ -0,0 +1,152 @@
+const S_page_title = "Configuration editor";
+
+const S_optional_step = "Optional step";
+const S_edit_prev_config = "Edit a previous configuration";
+
+const S_step_1 = "Step 1";
+const S_settings = "Settings";
+const S_controls = "Controls";
+const S_select_up_down = "Select = hold Up/Down";
+const S_select_reset = "Select = hold Reset";
+
+const S_step_2 = "Step 2";
+const S_choose_tools = "Choose included tools";
+const S_configure_tools = "Configure included tools";
+const S_acceleration_timer = "Acceleration timer";
+const S_oil_level_gauge = "Oil level gauge";
+const S_engine_mileage = "Engine mileage";
+const S_enabled = "Enabled";
+
+const S_step_3 = "Step 3";
+const S_choose_modules = "Choose modules for ECU data";
+
+const S_step_4 = "Step 4";
+const S_configure_ecu_data = "Configure ECU data";
+const S_pages = "Pages";
+const S_number = "Number";
+const S_title = "Title";
+const S_parameter_source = "Parameter source";
+const S_value_display = "Value display";
+const S_value_warning = "Value warning";
+const S_graph_parameters = "Graph parameters";
+const S_bottom = "Bottom";
+const S_top = "Top";
+const S_rate = "Rate";
+const S_parameters = "Parameters";
+const S_name = "Name";
+const S_units = "Units";
+
+const S_page_title_placeholder = "Page title";
+const S_parameter_name_placeholder = "Parameter name";
+const S_measurement_units_placeholder = "Measurement units";
+
+const S_step_5 = "Step 5";
+const S_download_config = "Download the current configuration";
+
+const HTML_strings = {
+  "S_optional_step":S_optional_step,
+  "S_edit_prev_config":S_edit_prev_config,
+  
+  "S_step_1":S_step_1,
+  "S_settings":S_settings,
+  "S_controls":S_controls,
+  "S_select_up_down":S_select_up_down,
+  "S_select_reset":S_select_reset,
+  
+  "S_step_2":S_step_2,
+  "S_choose_tools":S_choose_tools,
+  "S_configure_tools":S_configure_tools,
+  "S_acceleration_timer":S_acceleration_timer,
+  "S_oil_level_gauge":S_oil_level_gauge,
+  "S_engine_mileage":S_engine_mileage,
+  "S_enabled":S_enabled,
+  
+  "S_step_3":S_step_3,
+  "S_choose_modules":S_choose_modules,
+  
+  "S_step_4":S_step_4,
+  "S_configure_ecu_data":S_configure_ecu_data,
+  "S_pages":S_pages,
+  "S_number":S_number,
+  "S_title":S_title,
+  "S_parameter_source":S_parameter_source,
+  "S_value_display":S_value_display,
+  "S_value_warning":S_value_warning,
+  "S_graph_parameters":S_graph_parameters,
+  "S_bottom":S_bottom,
+  "S_top":S_top,
+  "S_rate":S_rate,
+  "S_parameters":S_parameters,
+  "S_name":S_name,
+  "S_units":S_units,
+  
+  "S_step_5":S_step_5,
+  "S_download_config":S_download_config,
+};
+
+const S_not_used = "Not used";
+const S_data_will_be_lost = "Data will be lost if you leave the page.";
+const S_speed_source = "Speed source";
+const S_oil_level_temperature_source = "Oil level/temperature source";
+const S_oil_level = "Oil level";
+const S_oil_temperature = "Oil temperature";
+const S_oil_level_values = "Oil level values";
+const S_mileage_source = "Mileage source";
+const S_mileage_offset = "Mileage offset";
+const S_protocol = "Protocol";
+const S_device = "Device";
+const S_block = "Block";
+const S_index = "Index";
+const S_min = "Min";
+const S_max = "Max";
+const S_are_you_sure_you_want_to_delete_page = "Are you sure you want to delete page ";
+const S_question = "? ";
+const S_this_cannot_be_undone = "This cannot be undone!";
+const S_are_you_sure_you_want_to_delete_parameter = "Are you sure you want to delete parameter ";
+const S_mode = "Mode";
+const S_forced_value = "Forced value";
+const S_decimals = "Decimals";
+const S_exact_value = "Exact value";
+const S_mapped_value = "Mapped value";
+const S_custom_text = "Custom text";
+const S_offset = "Offset";
+const S_in = "In";
+const S_out = "Out";
+const S_limit = "Limit";
+const S_condition = "Condition";
+const S_if_value_if_not_value = "If value / if not value";
+const S_if_inside_outside_range = "If inside / outside range";
+const S_if_below_inside_above_range = "If below / inside / above range";
+const S_value = "Value";
+const S_if_value = "If value";
+const S_if_not_value = "If not value";
+const S_equals_value = 'Equals value';
+const S_doesnt_equal_value = "Doesn\'t equal value";
+const S_inside = "Inside";
+const S_outside = "Outside";
+const S_between_min_max = "Between [Min, Max]";
+const S_outside_min_max = "Outside [Min, Max]";
+const S_range = "Range";
+const S_text = "Text";
+const S_less_min = "< min";
+const S_below_range = "Below range";
+const S_min_max = "[min, max]";
+const S_inside_range = "Inside range";
+const S_greater_max = "> max";
+const S_above_range = "Above range";
+const S_disabled = "Disabled";
+const S_if_exact_value = "If exact value";
+const S_if_not_exact_value = "If not exact value";
+const S_if_inside_range = "If inside range";
+const S_if_outside_range = "If outside range";
+
+const S_invalid_format = "Error: invalid file format!";
+const S_update_newer_version = "The configuration must be updated to a newer version.";
+const S_config_read_success = "The configuration has been read successfully.";
+const S_acceleration_tool_error = "Error: The acceleration timer tool is enabled but has an invalid speed source module!";
+const S_oil_level_tool_error = "Error: The oil level gauge tool is enabled but has an invalid oil level source module!";
+const S_mileage_tool_error = "Error: The engine mileage tool is enabled but has an invalid mileage source module!";
+const S_error_parameter = "Error: parameter ";
+const S_on_page = " on page ";
+const S_has_invalid_KWP_source = " has an invalid KWP source module!";
+const S_has_invalid_UDS_source = " has an invalid UDS source module!";
