1140b51d8e35dbd90a8ce76e4f3b741f372ab9da
CANFIS <147989168+CANFIS@users.noreply.github.com>
2025-04-28 20:40:14 +0300

    .

---
+++ new/EN/config_editor/config_updater/config_updater_strings_EN.js
@@ -0,0 +1,18 @@
+const S_page_title = "Configuration file updater";
+
+const S_load_config = "Load the old configuration file";
+const S_loaded = "Loaded: ";
+const S_none = "(none)";
+const S_download_config = "Download the new configuration file";
+
+const HTML_strings = {
+  "S_load_config":S_load_config,
+  "S_download_config":S_download_config,
+};
+
+const S_invalid_format = "Error: invalid file format!";
+const S_cant_update = "This configuration file cannot be updated.";
+const S_config_read_success = "The configuration has been read successfully.";
+const S_load_old_first = "Load the old configuration file first.";
+const S_select_target = "Select a target version.";
+const S_conversion_error = "An error occurred while converting the configuration file.";
---
+++ new/pl-PL/PL/config_editor/config_editor_strings_PL.js
@@ -0,0 +1,161 @@
+const S_page_title = "Edytor konfiguracji";
+
+const S_optional_step = "Opcjonalny krok";
+const S_edit_prev_config = "Edytuj plik konfiguracyjny";
+
+const S_step_1 = "Krok 1";
+const S_settings = "Ustawienia";
+const S_controls = "Sterowanie";
+const S_select_up_down = "Wybierz = przytrzymaj G\u00F3ra/D\u00F3\u0142";
+const S_select_reset = "Wybierz = przytrzymaj Reset";
+
+const S_step_2 = "Krok 2";
+const S_choose_tools = "Wybierz narz\u0119dzia dodatkowe";
+const S_configure_tools = "Skonfiguruj narz\u0119dzia dodatkowe";
+const S_acceleration_timer = "Licznik przy\u015Bpieszenia";
+const S_oil_level_gauge = "Miernik poziomu oleju";
+const S_engine_mileage = "Przebieg silnika";
+const S_openhaldex = "OpenHaldex";
+const S_enabled = "W\u0142\u0105czone";
+
+const S_step_3 = "Krok 3";
+const S_choose_modules = "Wybierz modu\u0142y do danych ECU";
+
+const S_step_4 = "Krok 4";
+const S_configure_ecu_data = "Skonfiguruj dane ECU";
+const S_pages = "Strony";
+const S_number = "Numer";
+const S_title = "Tytu\u0142";
+const S_options = "Opcje";
+const S_aligned_units = "Wyr\u00F3wnanie jednostki";
+const S_bar_at_bottom = "Pasek na dole";
+const S_parameter_source = "\u0179r\u00F3d\u0142o parametru";
+const S_value_display = "Wy\u015Bwietlanie warto\u015Bci";
+const S_value_warning = "Ostrze\u017Cenie o warto\u015Bci";
+const S_graph_parameters = "Parametry wykresu";
+const S_bottom = "Min. wart.";
+const S_top = "Maks. wart.";
+const S_rate = "Cz. odsw.";
+const S_parameters = "Parametry";
+const S_name = "Nazwa";
+const S_units = "Jednostki";
+
+const S_page_title_placeholder = "Tytu\u0142 strony";
+const S_parameter_name_placeholder = "Nazwa parametru";
+const S_measurement_units_placeholder = "Jednostki pomiaru";
+
+const S_step_5 = "Krok 5";
+const S_download_config = "Pobierz plik konfiguracyjny";
+
+const HTML_strings = {
+  "S_optional_step":S_optional_step,
+  "S_edit_prev_config":S_edit_prev_config,
+  
+  "S_step_1":S_step_1,
+  "S_settings":S_settings,
+  "S_controls":S_controls,
+  "S_select_up_down":S_select_up_down,
+  "S_select_reset":S_select_reset,
+  
+  "S_step_2":S_step_2,
+  "S_choose_tools":S_choose_tools,
+  "S_configure_tools":S_configure_tools,
+  "S_acceleration_timer":S_acceleration_timer,
+  "S_oil_level_gauge":S_oil_level_gauge,
+  "S_engine_mileage":S_engine_mileage,
+  "S_openhaldex":S_openhaldex,
+  "S_enabled":S_enabled,
+  
+  "S_step_3":S_step_3,
+  "S_choose_modules":S_choose_modules,
+  
+  "S_step_4":S_step_4,
+  "S_configure_ecu_data":S_configure_ecu_data,
+  "S_pages":S_pages,
+  "S_number":S_number,
+  "S_title":S_title,
+  "S_options":S_options,
+  "S_aligned_units":S_aligned_units,
+  "S_bar_at_bottom":S_bar_at_bottom,
+  "S_parameter_source":S_parameter_source,
+  "S_value_display":S_value_display,
+  "S_value_warning":S_value_warning,
+  "S_graph_parameters":S_graph_parameters,
+  "S_bottom":S_bottom,
+  "S_top":S_top,
+  "S_rate":S_rate,
+  "S_parameters":S_parameters,
+  "S_name":S_name,
+  "S_units":S_units,
+  
+  "S_step_5":S_step_5,
+  "S_download_config":S_download_config,
+};
+
+const S_not_used = "Nie u\u017Cywane";
+const S_data_will_be_lost = "Dane zostan\u0105 utracone, je\u015Bli opu\u015Bcisz stron\u0119.";
+const S_speed_source = "\u0179r\u00F3d\u0142o pr\u0119dko\u015Bci";
+const S_oil_level_temperature_source = "\u0179r\u00F3d\u0142o poziomu/temperatury oleju";
+const S_oil_level = "Poziom oleju";
+const S_oil_temperature = "Temperatura oleju";
+const S_oil_level_values = "Warto\u015Bci poziomu oleju";
+const S_mileage_source = "\u0179r\u00F3d\u0142o przebiegu";
+const S_mileage_offset = "Przesuni\u0119cie przebiegu";
+const S_protocol = "Protok\u00F3\u0142";
+const S_device = "Urz\u0105dzenie";
+const S_block = "Blok";
+const S_index = "Indeks";
+const S_measurement = "Pomiar";
+const S_min = "Min";
+const S_max = "Maks";
+const S_are_you_sure_you_want_to_delete_page = "Jeste\u015B pewny, \u017Ce chcesz usun\u0105\u0107 stron\u0119 ";
+const S_question = "? ";
+const S_this_cannot_be_undone = "Tej czynno\u015Bci nie mo\u017Cna cofn\u0105\u0107!";
+const S_are_you_sure_you_want_to_delete_parameter = "Jeste\u015B pewny, \u017Ce chcesz usun\u0105\u0107 parametr ";
+const S_mode = "Tryb";
+const S_forced_value = "Warto\u015B\u0107 wymuszona";
+const S_decimals = "Miejsca dziesi\u0119tne";
+const S_exact_value = "Warto\u015B\u0107 dok\u0142adna";
+const S_mapped_value = "Przemapowana warto\u015B\u0107";
+const S_custom_text = "Niestandardowy tekst";
+const S_offset = "Przesuni\u0119cie";
+const S_in = "Wej\u015Bcie";
+const S_out = "Wyj\u015Bcie";
+const S_limit = "Limit";
+const S_condition = "Warunek";
+const S_if_value_if_not_value = "Je\u015Bli warto\u015B\u0107 / je\u015Bli nie warto\u015B\u0107";
+const S_if_inside_outside_range = "Je\u015Bli wewn\u0105trz / poza zakresem";
+const S_if_below_inside_above_range = "Je\u015Bli poni\u017Cej / wewn\u0105trz / powy\u017Cej zakresu";
+const S_value = "Warto\u015B\u0107";
+const S_if_value = "Je\u015Bli warto\u015B\u0107";
+const S_if_not_value = "Je\u015Bli nie warto\u015B\u0107";
+const S_equals_value = 'R\u00F3wna si\u0119 warto\u015Bci';
+const S_doesnt_equal_value = "Nie r\u00F3wna si\u0119 warto\u015Bci";
+const S_inside = "Wewn\u0105trz";
+const S_outside = "Poza";
+const S_between_min_max = "Mi\u0119dzy [Min, Maks]";
+const S_outside_min_max = "Poza [Min, Maks]";
+const S_range = "Zakres";
+const S_text = "Tekst";
+const S_less_min = "< min";
+const S_below_range = "Poni\u017Cej zakresu";
+const S_min_max = "[min, maks]";
+const S_inside_range = "W zakresie";
+const S_greater_max = "> maks";
+const S_above_range = "Powy\u017Cej zakresu";
+const S_disabled = "Wy\u0142\u0105czone";
+const S_if_exact_value = "Je\u015Bli dok\u0142adna warto\u015B\u0107";
+const S_if_not_exact_value = "Je\u015Bli nie dok\u0142adna warto\u015B\u0107";
+const S_if_inside_range = "Je\u015Bli w zakresie";
+const S_if_outside_range = "Je\u015Bli poza zakresem";
+
+const S_invalid_format = "B\u0142\u0105d: nieprawid\u0142owy format pliku!";
+const S_update_newer_version = "Plik konfiguracyjny musi zosta\u0107 zaktualizowany do nowszej wersji.";
+const S_config_read_success = "Konfiguracja zosta\u0142a pomy\u015Blnie odczytana.";
+const S_acceleration_tool_error = "B\u0142\u0105d: Narz\u0119dzie licznika przy\u015Bpieszenia jest w\u0142\u0105czone, ale wybrany jest nieprawid\u0142owy modu\u0142 \u017Ar\u00F3d\u0142a pr\u0119dko\u015Bci!";
+const S_oil_level_tool_error = "B\u0142\u0105d: Narz\u0119dzie miernika oleju jest w\u0142\u0105czone, ale wybrany jest nieprawid\u0142owy modu\u0142 \u017Ar\u00F3d\u0142a poziomu oleju!";
+const S_mileage_tool_error = "B\u0142\u0105d: Narz\u0119dzie przebiegu silnika jest w\u0142\u0105czone, ale zosta\u0142 wybrany z\u0142y modu\u0142 \u017Ar\u00F3d\u0142a przebiegu!";
+const S_error_parameter = "B\u0142\u0105d: parametr ";
+const S_on_page = " na stronie ";
+const S_has_invalid_KWP_source = " ma nieprawid\u0142owy modu\u0142 \u017Ar\u00F3d\u0142owy KWP!";
+const S_has_invalid_UDS_source = " ma nieprawid\u0142owy modu\u0142 \u017Ar\u00F3d\u0142owy UDS!";
